967a94e50c16767554004507192c249def0397f2
Mickaël DEPARDON <mickael.depardon@wizbii.com>
2018-07-30 13:32:21 +0200

    refacor: remove checker db

--- 7.1/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-bash -c 'bash -s <<EOF
-      trap "break;exit" SIGHUP SIGINT SIGTERM
-      javac /opt/sonarqube/docker/com/basgeekball/db/Detector.java
-      java -cp `find /opt/sonarqube/ -name "mysql-connector*.jar"`:/opt/sonarqube/docker com.basgeekball.db.Detector && ./bin/run.sh
-EOF'
