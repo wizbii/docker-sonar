65e3b363322328588dba4b5ec7fcb9dc8d4212b6
Mickaël DEPARDON <squelix@users.noreply.github.com>
2018-07-30 10:48:22 +0200

    fix: error syntax

--- 7.1/docker-entrypoint.sh
+++ 7.1/docker-entrypoint.sh
@@ -4,4 +4,4 @@ bash -c 'bash -s <<EOF
       trap "break;exit" SIGHUP SIGINT SIGTERM
       javac /opt/sonarqube/docker/com/basgeekball/db/Detector.java
       java -cp `find /opt/sonarqube/ -name "mysql-connector*.jar"`:/opt/sonarqube/docker com.basgeekball.db.Detector && ./bin/run.sh
-      EOF'
+EOF'
